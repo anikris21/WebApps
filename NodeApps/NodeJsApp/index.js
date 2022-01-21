6fd65f6fc3b4b0a78fd346af0653af611cc4735c
Anil Kumar Konasale Krishna <anikris21@outlook.com>
2022-01-21 05:50:58 -0800

    Response - sending file

--- NodeApps/NodeJsApp/index.js
+++ NodeApps/NodeJsApp/index.js
@@ -1,4 +1,7 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const { dirname } = require('path/posix');
 
 const hostname = '127.0.0.1';
 const port = 8080;
@@ -7,8 +10,19 @@ const server = http.createServer((req, res) => {
 
   console.log("req ", req.url, req.method, req.httpVersion, req.headers.host);
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World! \n' + port);
+
+  const fileName = 'Readme.txt';
+  let filePath = path.join(__dirname, 'Readme.txt');
+  let stat = fs.statSync(filePath);
+
+  res.writeHead(200, {
+    "Content-Type": "application/octet-stream",
+    "Content-Disposition": "attachment; filename=" + fileName
+  });
+
+  fs.createReadStream(filePath).pipe(res);
+  // res.setHeader('Content-Type', 'text/plain');
+  // res.end('Hello World! \n' + port);
 });
 
 server.listen(port, hostname, () => {
